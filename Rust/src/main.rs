40f384abe6ce674a59d8ff9545ac9b48d1762057
kiralightyagami <crucisgamer98@gmail.com>
2026-08-17 16:37:53 +0530

    signup endpoint

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,14 +1,74 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, HttpResponse, web, App, HttpServer, Responder};
+
+#[derive(Serialize, Deserialize)]
+struct SignupInput {
+    pub username: String,
+    pub password: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct SignupResponse {
+    pub message: String,
+
+}
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {}!", name)
 }
 
+#[post("/signup")]
+async fn signup(body: json<SignupInput>, app_state: web::Data<AppState>) -> impl Responder {
+    let mut users = app_state.users.lock().unwrap();
+    let mut user_index = app_state.user_index.lock().unwrap();
+    
+    let user_found = users.iter().find(|u| u.username == body.username);
+
+    if user_found.is_none() {
+        *user_index = user_index + 1;
+
+    users.push(User {
+        id: user_index.clone(),
+        username: body.username.clone(),
+        password: body.password.clone()
+    });
+
+    drop(users);
+    HttpResponse::0k().json(SignupResponse {
+        message: String::from("signed up")
+    })
+    } else {
+        HttpResponse::Unauthorized().json(SignupResponse {
+            message: String::from("user already exists")
+        })
+    }
+    
+    
+}
+
+struct User {
+    id: u32,
+    username: String,
+    password: String
+}
+
+struct AppState {
+    user_index: Mutex<u32>,
+    users: Mutex<Vec<User>>
+}
+
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let app_state = web::Data::new(AppState{
+        users: Mutex::new(vec![]),
+        user_index:Mutex::new(0)
+    });
+    
+    HttpServer::new(move || {
         App::new().service(greet)
+        App::new()
+        .app_data(app_state.clone())
+        .service(signup)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- Ts/index.ts
+++ Ts/index.ts
@@ -2,6 +2,36 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json)
+
+let USER_INDEX = 0;
+
+const USERS: {
+    id: number,
+    username: string,
+    password: string
+}[] = [];
+
+app.post("/signup", (req,res) => {
+    const { username, password} = req.body;
+
+    if (USERS.find(u => u.username === username)) {
+        res.status(403).json({
+            message: "user not found"
+        })
+        return;
+    }
+
+    USERS.push({
+        id: USER_INDEX++,
+        username,
+        password
+    })
+
+    res.json({
+        message: "successfully signed up"
+    })
+})
 
 
 app.listen(3000);
